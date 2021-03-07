3dce915c6da357981eee9a42201fcf908e94ef33
hannawaffle <hanna.waffle@gmail.com>
2021-03-07 15:23:51 -0300

    migrate to react

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './mvp.css';
+import './shake.css';
+
+const App = () => (
+  <>
+    <header>
+      <nav>
+        <a href="/">🧇 WaffleDefi</a>
+        <ul>
+          <li>Home</li>
+          <li><a href="https://github.com/waffledefi/" target="_blank" rel="noreferrer">Github</a></li>
+        </ul>
+      </nav>
+      <h1><span class="shake">🧇</span> WaffleDefi</h1>
+      <p>Feel the crunchiness</p>
+    </header>
+
+    <main>
+      <hr/>
+      <section>
+        <blockquote>
+          Auto Market Maker
+          <footer><i>Launching soon</i></footer>
+        </blockquote>
+      </section>
+    </main>
+
+    <footer>
+      <hr/>
+      <p>
+        <small>WaffleDefi</small>
+      </p>
+    </footer>
+  </>
+)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
